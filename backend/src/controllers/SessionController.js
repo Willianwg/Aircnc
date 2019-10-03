4cb3121c6376c97123773faacb0377171409bba6
willian <willian.wg70@gmail.com>
2019-10-03 01:59:30 -0300

    backend pronto

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,23 @@
+const User = require ("../models/User");
+
+module.exports={
+	
+	async index(req, res){
+		const user= await User.find();
+		
+		return res.json(user);
+	},
+	
+	async store(req, res){
+		const { email } = req.body;
+		let user = await User.findOne({ email });
+		
+		if(!user){
+			user = await User.create({ email });
+		};
+		
+		return res.json(user);
+	},
+	
+	
+};
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express= require ("express");
+const mongoose= require("mongoose");
+const routes = require("./routes");
+const app= express();
+
+
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb+srv://user:qwer1245@cluster0-x73mg.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true });
+
+
+app.use( express.json() );
+app.use(routes);
+
+app.listen(3001);
